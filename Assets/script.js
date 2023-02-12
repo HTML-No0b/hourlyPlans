9c67dc5012919b2c57f6c5e27a73dead982ca084
Html-No0b <rmartin9797@gmail.com>
2023-02-11 20:10:57 -0600

    added local storage saves to button

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,24 @@
-var today = moment().format('dddd, MMM Do YYYY')
-$("#currentDay").text(today.format('MMM, DO YYYY'))
+var today = moment().format("dddd, MMM Do YYYY");
+$("#currentDay").text(today.format("LLLL"));
 
 
-$('.saveBtn').on("click",function(event){
 
-    var time = $(this).siblings(".time-block").val()
+
+$(".saveBtn").on("click", function () {
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, text);
+});
+
+$("#hour8 .timeblock").val(localStorage).getItem("hour8");
+$("#hour9 .timeblock").val(localStorage).getItem("hour9");
+$("#hour10 .timeblock").val(localStorage).getItem("hour10");
+$("#hour11 .timeblock").val(localStorage).getItem("hour11");
+
+function timetracker(){
+var liveHours = today.hours();
+$('.time-block').each(function(){
+    var loopId = parseInt($(this).attr('id').split('hour')[1]);
 })
+
+}
