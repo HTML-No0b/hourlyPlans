ae3dd4835f967ff69f4a178b4004576efefac314
Html-No0b <rmartin9797@gmail.com>
2023-02-11 20:21:47 -0600

    added if statements

--- Assets/script.js
+++ Assets/script.js
@@ -10,15 +10,25 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(time, text);
 });
 
-$("#hour8 .timeblock").val(localStorage).getItem("hour8");
-$("#hour9 .timeblock").val(localStorage).getItem("hour9");
-$("#hour10 .timeblock").val(localStorage).getItem("hour10");
-$("#hour11 .timeblock").val(localStorage).getItem("hour11");
-
 function timetracker(){
-var liveHours = today.hours();
+var liveHour = today.hours();
 $('.time-block').each(function(){
     var loopId = parseInt($(this).attr('id').split('hour')[1]);
 })
+if (loopId<currentHour) {
+    $(this).addClass('past');
+}
+else if (loopID === currentHour){
+    $(this).removeClass('past')
+    $(this).removeClass('future')
+    $(this).addClass('present')
+
 
 }
+
+}
+
+$("#hour8 .description").val(localStorage).getItem("hour8");
+$("#hour9 .description").val(localStorage).getItem("hour9");
+$("#hour10 .description").val(localStorage).getItem("hour10");
+$("#hour11 .description").val(localStorage).getItem("hour11");
