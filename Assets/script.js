8fe0e268d9450f56bc6c5b0e6b0293e015651ad5
Html-No0b <rmartin9797@gmail.com>
2023-02-11 19:55:00 -0600

    added code to js script

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,8 @@
-var todaysDate = moment().format('dddd,MMM Do YYYY')
-$('#currentDay').html(todaysDate)
+var today = moment().format('dddd, MMM Do YYYY')
+$("#currentDay").text(today.format('MMM, DO YYYY'))
+
+
+$('.saveBtn').on("click",function(event){
+
+    var time = $(this).siblings(".time-block").val()
+})
